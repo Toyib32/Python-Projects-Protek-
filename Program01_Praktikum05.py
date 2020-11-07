23cb27c5ae05d60d6db77d66cd81ca2b141f531e
Toyib32 <71622778+Toyib32@users.noreply.github.com>
2020-11-07 16:38:29 +0700

    Create Program01_Praktikum05.py

---
+++ Program01_Praktikum05.py
@@ -0,0 +1,19 @@
+# lakukan input
+bIndo = int(input("Masukkan nilai Bhs Indonesia     :"))
+ipa = int(input("Masukkan nilai IPA               :"))
+matematika = int(input("Masukkan nilai Matematika        :"))
+
+# lakukan cetak
+print("-------------------------------------"+"\n")
+if(bIndo >= 60) and (ipa >= 60) and (matematika >= 70):
+    print("Status Kelulusan                 : LULUS")
+else:
+    print("Status Kelulusan                 : TIDAK LULUS")
+
+
+
+
+
+
+
+
