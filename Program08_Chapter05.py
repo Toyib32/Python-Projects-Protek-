13b3fac381e815213dd94dd059739c71ebe6286a
Toyib32 <71622778+Toyib32@users.noreply.github.com>
2020-11-07 16:38:56 +0700

    Create Program08_Chapter05.py

---
+++ Program08_Chapter05.py
@@ -0,0 +1,10 @@
+a = 0
+b = 1
+c = 0
+while(b <= 100):
+    print(b)
+    c = c + b
+    b += 2
+    a += 1
+print("banyaknya bilangan ganjil : "+ str(a))
+print("jumlah seluruh bilangan   : "+ str(c))
