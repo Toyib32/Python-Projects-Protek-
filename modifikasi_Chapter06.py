9c9fff32deac17919ca59caa8c594b8eb4c9cc9b
Toyib32 <71622778+Toyib32@users.noreply.github.com>
2020-11-09 23:14:18 +0700

    Create modifikasi_Chapter06.py

---
+++ modifikasi_Chapter06.py
@@ -0,0 +1,14 @@
+def luasSegitiga(a, t):
+    luas = a*t/2
+    return luas
+
+alas1 = 10
+tinggi1= 20
+alas2 = 15
+tinggi2 = 45
+print("luas segitiga dengan alas", alas1,
+      "dan tinggi", tinggi1,
+      "adalah", luasSegitiga(alas1,tinggi1))
+print("luas segitiga dengan alas", alas2,
+      "dan tinggi", tinggi2,
+      "adalah", luasSegitiga(alas2,tinggi2))
