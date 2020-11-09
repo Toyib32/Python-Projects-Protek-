b1ff7d69892642b6d613cb6075a4c2ee088063ec
Toyib32 <71622778+Toyib32@users.noreply.github.com>
2020-11-09 23:14:35 +0700

    Create Test4_Chapter06_s.py

---
+++ Test4_Chapter06_s.py
@@ -0,0 +1,18 @@
+from operation import *
+
+hasilSatu = kali(4, 6)
+hasilDua = jumlah(2, hasilSatu)
+hasilAkhir = kurang(hasilDua, 4)
+print("jawaban a. ",(hasilAkhir))
+
+a = jumlah(4, 7)
+b = kurang(6, 9)
+c = kali(a, b)
+print("jawaban b. ",(c))
+
+d = jumlah(10, 2)
+e = jumlah(7, 5)
+f = kurang(12, 34)
+g = bagi(d, e)
+h = bagi(g, f)
+print("jawaban c. ",(h))
