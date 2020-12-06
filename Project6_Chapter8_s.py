26b8c57d076349981e5effcca37f9581329a1f71
Toyib32 <71622778+Toyib32@users.noreply.github.com>
2020-12-06 23:11:17 +0700

    Create Project6_Chapter8_s.py

---
+++ Project6_Chapter8_s.py
@@ -0,0 +1,9 @@
+def sortStringByChar (mydataku):
+    mydataku.sort(reverse=True)
+    return mydataku
+
+mydataku = ["apel", "rambutan", "jeruk"]
+dataBaru = sortStringByChar (mydataku)
+
+print ("Data Baru : ", dataBaru)
+    
