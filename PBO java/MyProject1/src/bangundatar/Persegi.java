3999cbf45ecdf1566cb3677007de0b3cc9c7bc7f
Toyib32 <71622778+Toyib32@users.noreply.github.com>
2021-09-22 21:36:16 +0700

    Create Persegi.java

---
+++ PBO java/MyProject1/src/bangundatar/Persegi.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bangundatar;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Persegi {
+    //atribut 
+    public String namaPersegi;
+    public int sisi;
+    
+    //method 
+    public void hitungLuasPersegi(){
+        int luas = sisi * sisi;
+        System.out.println("Luas Persegi " + namaPersegi + " adalah ; " + luas);
+    }
+    
+    public void hitungKelilingPersegi(){
+        int keliling = 4 * sisi;
+        System.out.println("Keliling persegi " + namaPersegi + " adalah ;" + keliling);
+    }
+}
